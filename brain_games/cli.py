36470dde366bd45e956675a402e724b6024c41b9
ZIma <barne39@gmail.com>
2021-03-22 17:04:26 +0300

    step 3

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,12 +1,17 @@
+"""Модуль приветствия."""
 import prompt
 
+
 def welcome_user():
-    name = prompt.string("May I have your name? ")
-    print("Hello, {}!".format(name))
+    """Узнай имя, выведи его."""
+    name = prompt.string('May I have your name? ')
+    print('Hello, {}!'.format(name))
+
 
 def main():
+    """Выполни функцию."""
     welcome_user()
+
 
 if __name__ == '__main__':
     main()
-
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,18 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.9
 
-from brain_games.cli  import welcome_user
+"""test."""
+
+from brain_games.cli import welcome_user
 
 
 def greet():
-    print("Welcome to the Brain Games!")
+    """Вызови функцию print."""
+    print('Welcome to the Brain Games!')
+
 
 def main():
+    """Выполни main."""
     greet()
     welcome_user()
 
+
 if __name__ == '__main__':
     main()
-
-
-
-
